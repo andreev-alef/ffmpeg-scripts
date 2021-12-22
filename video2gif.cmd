ee2ede9490a9111dc885e4871d8e522c50e9b5b9
Alexander Andreev <andreev-alef@users.noreply.github.com>
2021-12-22 17:17:27 +0300

    Update video2gif.cmd

--- video2gif.cmd
+++ video2gif.cmd
@@ -2,4 +2,4 @@ set palette="palette.png"
 set filters="fps=20,scale=360:-1:flags=lanczos"
 
 ffmpeg -i %1 -vf ^"%filters%,palettegen^" -y %palette%
-ffmpeg -i %1 -i %palette% -lavfi ^"%filters% [x]; [x][1:v] paletteuse^" -y %2
+ffmpeg -i %1 -i %palette% -lavfi ^"%filters% [x]; [x][1:v] paletteuse^" -y "%~n1.gif"
