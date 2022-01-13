065396bc42efea15dde839b58ef073f77eba1647
Alexander Andreev <andreev-alef@users.noreply.github.com>
2022-01-13 15:18:35 +0300

    Наложение картинки на видео

--- create_overlay.cmd
+++ create_overlay.cmd
@@ -1,1 +1,1 @@
-ffmpeg -i "исходное видео.mp4" -i фон.png -filter_complex "[0:v][1:v]overlay=X_фона:Y_фона" "выходное видео.mp4"
+ffmpeg -i "исходное видео.mp4" -i фон.png -filter_complex "[0:v][1:v]overlay=X_фона:Y_фона" -preset ultrafast -threads 4 "выходное видео.mp4"
